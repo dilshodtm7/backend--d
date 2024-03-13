2cbff62742c839c490f37da428bd7a3157955e56
dilshodtm7 <155984333+dilshodtm7@users.noreply.github.com>
2024-03-13 19:30:38 +0600

    Update db.sql

--- src/db/db.sql
+++ src/db/db.sql
@@ -3,9 +3,7 @@ CREATE DATABASE winfake;
 
 CREATE TABLE login(
     id SERIAL PRIMARY KEY,
-    username VARCHAR(10),
-    password VARCHAR(12),
     email VARCHAR(24),
-    winid VARCHAR(12),
+    password VARCHAR(12), 
     status VARCHAR(16) default "inactive"
 )
